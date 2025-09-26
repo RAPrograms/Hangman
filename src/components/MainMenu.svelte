2a9765898e253ecff5fa0780f7a0e4dab60f5512
Dog2657 <RAPrograms@outlook.com>
2025-09-26 07:15:09 +0100

    Created basic layout

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -1,67 +1,42 @@
 <div class="screen">
-    <main>
-        <h2>Game Modes</h2>
-        <button type="button" class="custom">Custom Word</button>
-        <button type="button" class="category">Category Random</button>
-        <button type="button" class="random">Random</button>
-        <br>
-        <button type="button" class="word-bank">Word Bank</button>
-        <button type="button" class="settings">Settings</button>
-    </main>
-
-    <h1>Hangman</h1>
-
-    <footer>Made by
-        <a href="https://github.com/RAPrograms">RAPrograms</a>
-    </footer>
+    <main></main>
 </div>
 
 <style lang="scss">
+    
     .screen{
+        $sidebar-width: 380px;
 
-
-        @media (width > 0px) {
-            display: grid; 
-            grid-auto-columns: 1fr; 
-            grid-template-columns: 1fr 1fr; 
+        @media (width >= ($sidebar-width * 2)) {  
+            grid-template-columns: $sidebar-width 1fr; 
             grid-template-rows: 1fr min-content; 
+            grid-auto-columns: 1fr; 
+            position: relative;
+            isolation: isolate;
+            display: grid; 
             gap: 0px 0px; 
             grid-template-areas: 
                 "Modes Logo"
                 "Modes Credit"; 
+
+
+            &::before{
+                background-image: url("/assets/background.jpeg");
+                background-repeat: no-repeat;
+                background-position: bottom;
+                filter: brightness(30%);
+                background-size: cover;
+                position: absolute;
+                height: 100%;
+                width: 100%;
+                z-index: -1;
+                content: "";
+            }
         }
 
         & > main{
+            background: var(--background-colour);
             grid-area: Modes;
-            max-width: 380px;
-            display: flex;
-            flex-direction: column;
-            padding: 20px;
-            justify-content: center;
-            gap: 20px;
-
-            border-right: 1px solid color-mix(in oklab, grey 20%, transparent);
-
-            & > :first-child::after{
-                content: "";
-                height: 5px;
-                width: 20%;
-                background-color: white;
-                display: block;
-                border-radius: 20px;
-            }
-
-            & > button{
-                transition: scale 400ms ease-out;
-                border-radius: 10px;
-                cursor: pointer;
-                height: 64px;
-
-                &:hover{
-                    transition: scale 250ms ease-out;
-                    scale: 1.03;
-                }
-            }
         }
     }
 </style>
