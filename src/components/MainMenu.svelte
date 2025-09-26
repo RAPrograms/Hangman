3058e2215ba25dada8a4da276603820bde843fc4
Dog2657 <RAPrograms@outlook.com>
2025-09-26 20:30:04 +0100

    Added buttons

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -1,10 +1,18 @@
 <div class="screen">
-    <main></main>
-
     <div class="logo">
         <h1>Hangman</h1>
         <img src="/assets/logo.svg" alt="">
     </div>
+
+    <main>
+        <h2>Game Modes</h2>
+        <button type="button" class="custom">Custom Word</button>
+        <button type="button" class="category">Category Random</button>
+        <button type="button" class="random">Random</button>
+        <br>
+        <button type="button" class="word-bank">Word Bank</button>
+        <button type="button" class="settings">Settings</button>
+    </main>
 
     <footer>
         <a href="https://github.com/RAPrograms/Hangman">RAPrograms</a>
@@ -52,6 +60,31 @@
         & > main{
             background: var(--background-colour);
             grid-area: Modes;
+            padding: 10px;
+            gap: 20px;
+
+            @media (width >= ($sidebar-width * 2)){
+                border-right: 1px solid rgba(128, 128, 128, 0.505);
+            }
+
+            & > :first-child{
+                @media (width >= ($sidebar-width * 2)){
+                    &::after{
+                        background-color: white;
+                        border-radius: 20px;
+                        margin-bottom: 10px;
+                        display: block;
+                        content: "";
+                        height: 5px;
+                        width: 20%;
+                    }
+                }
+
+                @media (width < ($sidebar-width * 2)){
+                    visibility: none;
+                    display: none;
+                }
+            }
         }
 
         & > .logo{
