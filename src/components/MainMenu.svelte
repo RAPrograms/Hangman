f8a79837aef50241cf56add7765d5d99e2d31179
Dog2657 <RAPrograms@outlook.com>
2025-09-26 07:32:19 +0100

    Added logo to main menu

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -1,5 +1,14 @@
 <div class="screen">
     <main></main>
+
+    <div class="logo">
+        <h1>Hangman</h1>
+        <img src="/assets/logo.svg" alt="">
+    </div>
+
+    <footer>
+        <a href="https://github.com/RAPrograms/Hangman">RAPrograms</a>
+    </footer>
 </div>
 
 <style lang="scss">
@@ -34,9 +43,49 @@
             }
         }
 
+        & > main, & > .logo{
+            justify-content: center;        
+            flex-direction: column;
+            display: flex;
+        }
+
         & > main{
             background: var(--background-colour);
             grid-area: Modes;
+        }
+
+        & > .logo{
+            align-items: center;
+            grid-area: Logo;
+
+            & > h1{
+                text-transform: uppercase;
+                letter-spacing: 2%;
+
+                @media (width >= ($sidebar-width * 2)){
+                    font-size: min(8vw, 100px);
+                }
+            }
+
+            & > img{
+                @media (width < ($sidebar-width * 2)) or (height < 600px){
+                    visibility: none;
+                    display: none;
+                }
+
+                width: 320px;
+            }
+        }
+
+        & > footer{
+            text-align: center;
+            font-weight: bold;
+            grid-area: Credit;
+            padding: 10px 0;
+            
+            & > a{
+                color: white;
+            }
         }
     }
 </style>
