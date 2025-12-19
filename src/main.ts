53baf4ef34726c439059b2330ed7a927b7373ee5
RAPrograms <RAPrograms@outlook.com>
2025-12-19 18:41:36 +0000

    Moved card styling into mixin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { mount } from 'svelte'
 import App from './App.svelte'
-import "./main.scss"
+import "$styles/main.scss"
 
 const app = mount(App, {
   target: document.getElementById('app')!,
