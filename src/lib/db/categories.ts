765cd4ea5f090e537cf70a236fb6a315971f44ad
RAPrograms <RAPrograms@outlook.com>
2025-12-21 00:22:15 +0000

    Added word deletion to category class

--- src/lib/db/categories.ts
+++ src/lib/db/categories.ts
@@ -23,20 +23,21 @@ export default class WordCategory{
         return new this(name, size, db)
     }
 
-    async addWords(value: Array<string> | string, local_only: boolean = true){
+    async addWords(value: Array<string> | string){
         if(typeof value == "string")
             value = [value]
 
         // Ensures to add category to categories store
         if((await this.#db.get("categories", this.#name)) == undefined)
-            await this.#db.add("categories", { name: this.#name, local_only })
+            await this.#db.add("categories", { name: this.#name })
 
         const index = this.#db.transaction('words').store.index('hash');
         
         const tasks = []
-        for (const word of value){
-            tasks.push(new Promise<void>(async (resolve) => {
+        for (let word of value){
+            word = word.toLowerCase()
 
+            tasks.push(new Promise<void>(async (resolve) => {
                 // Presence check to prevent duplicates
                 if(await index.get([this.#name, word]))
                     return resolve()
@@ -52,7 +53,39 @@ export default class WordCategory{
         this.#size += value.length
     }
 
+    async removeWords(value: Array<string> | string){
+        if(typeof value == "string")
+            value = [value]
+
+        // Creates a hashmap of all lowercase words to be removed
+        const map = new Set(value.map(val => val.toLowerCase()))
+
+        const tasks = []
+
+        for await (const cursor of this.#reader()) {
+            if(!map.has(cursor.value.content))
+                continue
+
+            tasks.push(this.#db.delete("words", cursor.primaryKey))
+        }
+
+        await Promise.all(tasks)
+
+        this.#size -= value.length
+        if(this.#size > 0)
+            return
+
+        await this.#db.delete("categories", this.#name)
+    }
+
     get size(){
         return this.#size
+    }
+
+    async* #reader(){
+        const index = this.#db.transaction('words').store.index('category');
+        for await (const cursor of index.iterate(this.#name)) {
+            yield cursor
+        }
     }
 }
