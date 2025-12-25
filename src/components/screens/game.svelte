7ba3ea12af19c9962bfff75a1da8bb5749b6bbc4
RAPrograms <RAPrograms@outlook.com>
2025-12-25 15:17:37 +0000

    Updated game layout

--- src/components/screens/game.svelte
+++ src/components/screens/game.svelte
@@ -20,98 +20,142 @@
     }
 </script>
 
-<main>
-    <header>
-        <h1>Hangman</h1>
-        <p>Guess the word before you run out of attempts</p>
-    </header>
+<div class="game">
 
-    <section>
-        <div>Remaining Attempts</div>
-        <div>{lives}</div>
-        <div>
-            {#each {length: total_lives} as _, i}
-                {@const used = (i + 1) <= lives}
-                <span class:used={!used}>
-                    {#if used} 
-                        ❤️
-                    {:else}
-                        💔
-                    {/if}
-                </span>
-            {/each}
-        </div>
-    </section>
-</main>
+    <main>
+        <header>
+            <h1>Hangman</h1>
+            <p>Guess the word before you run out of attempts</p>
+        </header>
 
-<div>{word} from {category}</div>
+        <section>
+            <div>Remaining Attempts</div>
+            <div>{Math.max(lives, 0)}</div>
+            <div>
+                {#each {length: total_lives} as _, i}
+                    {@const used = (i + 1) <= lives}
+                    <span class:used={!used}>
+                        {#if used} 
+                            ❤️
+                        {:else}
+                            💔
+                        {/if}
+                    </span>
+                {/each}
+            </div>
+        </section>
+    </main>
+    
+    <Keyboard validator={validateLetter}/>
+</div>
 
-<Keyboard validator={validateLetter}/>
 
 <style lang="scss">
     @use "../../styling/variables" as *;
 
-    main{
-        & > header{
+    div.game{
+        justify-content: space-between;
+        min-height: 100vh;
+        padding: 20px 0;
+
+        &, & > main, & > main > header{
             flex-direction: column;
             display: flex;
-            gap: 10px;
-
-            & > h1{
-                font-size: 1.3rem;
-            }
         }
 
-        & > section{
-            @include UI_Card($background-opacity: 40%);
+        & > main{
+            justify-content: center;
+            align-items: center;
+            flex-grow: 1;
+            gap: 30px;
 
-            display: grid; 
-            grid-auto-columns: 1fr; 
-            grid-template-columns: max-content 1fr; 
-            grid-template-rows: max-content 1fr; 
-            padding: 20px;
-            gap: 10px; 
-            grid-template-areas: 
-                "Title Title"
-                "Number Symboles"; 
-
-            & > :nth-child(1){
-                grid-area: Title;
-                text-align: left;
-                font-size: 1.3rem;
-                color: var(--faint-colour);
-            }
-
-            & > :nth-child(2){
-                grid-area: Number;
-                font-size: 2rem;
-            }
-
-            & > :nth-child(3){
-                justify-content: right;
-                align-items: center;
-                grid-area: Symboles;
-                text-align: right;
+            & > header{
+                flex-direction: column;
+                margin-bottom: 20px;
                 display: flex;
-                gap: 7px;
+                gap: 10px;
 
-                & > *{
-                    outline: 1px solid color-mix(in hsl shorter hue, var(--faint-colour) 20%, transparent);
-                    border-radius: 100%;
-                    aspect-ratio: 1/1;
-                    padding: 5px;
-                    font-size: 1rem;
-                    height: 2rem;
-                    display: grid;
-                    place-items: center;
-                    line-height: 0;
+                & > h1{
+                    font-size: 1.9rem;
+                }
 
-                    &:not(.used){
-                        background-color: color-mix(in hsl shorter hue, rgb(0, 77, 170) 50%, transparent);
+                & > p{
+                    color: var(--faint-colour);
+                }
+            }
+
+            & > section{
+                @include UI_Card($background-opacity: 40%);
+
+                min-width: min(95vw, 500px);
+                max-width: 95vw;
+                padding: 20px;
+                gap: 10px; 
+
+
+                @media (width >= 400px) {
+                    grid-template-columns: max-content 1fr; 
+                    grid-template-rows: max-content 1fr; 
+                    grid-auto-columns: 1fr; 
+                    display: grid; 
+                    grid-template-areas: 
+                        "Title Title"
+                        "Number Symboles"; 
+                }
+
+                @media (width < 400px) {
+                    flex-direction: column;
+                    display: flex;
+                }
+
+                & > :nth-child(1){
+                    color: var(--faint-colour);
+                    font-size: 1.3rem;
+                    grid-area: Title;
+                    
+                    @media (width >= 400px) {
+                        text-align: left;
+                    }
+                }
+
+                & > :nth-child(2){
+                    grid-area: Number;
+                    font-size: 2rem;
+                }
+
+                & > :nth-child(3){
+                    align-items: center;
+                    grid-area: Symboles;
+                    text-align: right;
+                    display: flex;
+                    gap: 7px;
+                    
+                    @media (width >= 400px) {
+                        justify-content: right;
                     }
 
-                    &.used{
-                        background-color: rgb(21, 28, 40);
+                    @media (width < 400px) {
+                        justify-content: center;
+                    }
+
+                    & > *{
+                        outline: 1px solid color-mix(in hsl shorter hue, var(--faint-colour) 20%, transparent);
+                        border-radius: 100%;
+                        aspect-ratio: 1/1;
+                        padding: 5px;
+                        font-size: 1rem;
+                        height: 2rem;
+                        display: grid;
+                        place-items: center;
+                        line-height: 0;
+
+                        &:not(.used){
+                            background-color: color-mix(in hsl shorter hue, rgb(0, 77, 170) 50%, transparent);
+                        }
+
+                        &.used{
+                            background-color: rgb(21, 28, 40);
+                        }
                     }
                 }
             }
