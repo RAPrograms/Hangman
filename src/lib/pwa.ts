d52e4b2c0617638481e544216c836806ff8289ae
RAPrograms <RAPrograms@outlook.com>
2025-12-26 15:27:09 +0000

    Added svelte store to hide pwa install button when installed

--- src/lib/pwa.ts
+++ src/lib/pwa.ts
@@ -1,2 +1,21 @@
-//console.log("serviceWorker" in navigator)
-navigator.serviceWorker.register("")
+import { writable } from 'svelte/store';
+
+export const pwaInstalled = (() => {
+    const { set, subscribe } = writable(
+        // Standard PWA check (Chrome, Edge, Android, modern Safari)
+        window.matchMedia("(display-mode: standalone)").matches ||
+
+        // iOS Safari legacy check
+        // @ts-ignore
+        window.navigator.standalone == true ||
+
+        // Android Trusted Web Activity / WebAPK
+        document.referrer.startsWith("android-app://")
+    )
+
+    window.addEventListener("appinstalled", () => {
+        set(true)
+    })
+
+    return { subscribe }
+})()
